7d9fee4a41b7c6317bfe4864a1954ff826aab3d4
Mike <zml2416154671@gmail.com>
2023-07-14 16:35:00 +0800

    reset config

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import { logo, menu, close } from "../assets";
 const Navbar = () => {
     const [active, setActive] = useState("");
     const [toggle, setToggle] = useState(false);
-    const [scrolled, setScrolled] = useState(false);
+    const [scrolled, setScrolled] = useState(true);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -27,7 +27,11 @@ const Navbar = () => {
 
     return (
         <nav
-            className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 `}
+            className={`${
+                styles.paddingX
+            } w-full flex items-center py-5 fixed top-0 z-20 ${
+                scrolled ? "bg-primary" : "bg-transparent"
+            }`}
         >
             <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
                 <Link to="/" className="flex items-center gap-2">
