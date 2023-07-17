5b986ed0415ae42e74fd200bc2836d72a0656aff
Mike <zml2416154671@gmail.com>
2023-07-17 23:20:54 +0800

    add auto scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,13 @@ import {
     Tech,
     Works,
     StarsCanvas,
+    ScrollToTop,
 } from "./components";
 
 const App = () => {
     return (
         <BrowserRouter>
-            {/* <ToastContainer> */}
+            <ScrollToTop />
             <div className="relative z-0 bg-primary">
                 <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
                     <Navbar />
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import {
+    EarthCanvas,
+    BallCanvas,
+    ComputersCanvas,
+    StarsCanvas,
+} from "./canvas";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
@@ -8,20 +13,21 @@ import Works from "./Works";
 import Feedbacks from "./Feedbacks";
 import Contact from "./Contact";
 import CanvasLoader from "./Loader";
-
+import ScrollToTop from "./ScrollToTop";
 
 export {
-  Hero,
-  Navbar,
-  About,
-  Tech,
-  Experience,
-  Works,
-  Feedbacks,
-  Contact,
-  CanvasLoader,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
-  StarsCanvas
+    Hero,
+    Navbar,
+    About,
+    Tech,
+    Experience,
+    Works,
+    Feedbacks,
+    Contact,
+    CanvasLoader,
+    EarthCanvas,
+    BallCanvas,
+    ComputersCanvas,
+    StarsCanvas,
+    ScrollToTop,
 };
